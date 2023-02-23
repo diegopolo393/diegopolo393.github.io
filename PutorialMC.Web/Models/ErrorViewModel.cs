73fab64cf2c53b38aa2570e526b15a8c75a1f8ee
diegopolo393 <diegopolo393@gmail.com>
2023-02-23 11:49:40 -0600

    commit

---
+++ PutorialMC.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PutorialMC.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# diegopolo393.github.io
